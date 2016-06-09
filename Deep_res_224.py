efbfab36b69ba0aabafd2df19b22e865fbfbb380
Martijn Nanne <martijn.nanne@student.ru.nl>
2016-06-09 15:47:40 +0200

    lasagne

--- Deep_res_224.py
+++ Deep_res_224.py
@@ -191,8 +191,11 @@ def main(n=5, num_epochs=82, model=None):
     network = build_cnn(input_var, n)
     print("number of parameters in model: %d" % lasagne.layers.count_params(network, trainable=True))
 
-
-    if model==None:
+    print("load params..")
+    with np.load(model) as f:
+         param_values = [f['arr_%d' % i] for i in range(len(f.files))]
+    lasagne.layers.set_all_param_values(network, param_values)
+    if True:
         # Create a loss expression for training, i.e., a scalar objective we want
         # to minimize (for our multi-class problem, it is the cross-entropy loss):
         prediction = lasagne.layers.get_output(network)
@@ -236,7 +239,7 @@ def main(n=5, num_epochs=82, model=None):
     print("compile test function")
     test_fn = theano.function([input_var], test_prediction)
 
-    if model==None:
+    if True:
         # # launch the training loop
         print("Starting training...")
         #We iterate over epochs:
@@ -329,4 +332,4 @@ if __name__ == '__main__':
             kwargs['model'] = sys.argv[2]
         #main(**kwargs)
         #main(5,2,"cifar_model_n5.npz")
-        main(9,5, "cifar10_deep_residual_model_224.npz")
+        main(9,5, "cifar_model_n9.npz")
