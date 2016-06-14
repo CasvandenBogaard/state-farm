b536d07ccf3fa6dbc612eef8592914de39bc3ec9
Martijn Nanne <martijn.nanne@student.ru.nl>
2016-06-14 10:47:18 +0200

    lasagne

--- Deep_res_224.py
+++ Deep_res_224.py
@@ -258,7 +258,7 @@ def main(n=5, num_epochs=82, model=None):
             train_batches = 0
             start_time = time.time()
             print("START BATCHING!")
-            for batch in iterate_minibatches(X_train, Y_train, 4, shuffle=True, augment=True):
+            for batch in iterate_minibatches(X_train, Y_train, 8, shuffle=True, augment=True):
                 inputs, targets = batch
                 train_err += train_fn(inputs, targets)
                 train_batches += 1
